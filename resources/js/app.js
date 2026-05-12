20c19511aeffae5337818c217d2f385b09cd2b3f
Tabitha Chebet <tabithasabila48@gmail.com>
2026-05-12 12:18:20 +0300

    Added missing UI features to the admin booking screen, Finished UI for admin dashboard

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,12 @@ import Alpine from 'alpinejs';
 
 import vehicleManager from './vehicleManager';
 
+import bookingManager from './bookingManager';
+
 window.Alpine = Alpine;
 
 // Register the data component BEFORE starting Alpine
 Alpine.data('vehicleManager', vehicleManager);
+Alpine.data('bookingManager', bookingManager);
 
 Alpine.start();
