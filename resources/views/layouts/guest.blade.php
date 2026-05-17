ecd1c4214aaace61903c4d3a8fcfa9fab344430e
Tabitha Chebet <tabithasabila48@gmail.com>
2026-05-17 23:21:18 +0300

    Updated the login card layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,11 +14,12 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://unpkg.com/lucide@latest"></script>
 </head>
 <body class="font-sans text-gray-900 antialiased">
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
         
-        <div>
+<div class="flex flex-col items-center">
     <a href="">
         <img 
             src="{{ asset('images/Favicon.png') }}" 
@@ -26,11 +27,18 @@
             class="w-20 h-20"
         >
     </a>
+    <p class="text-xl font-bold text-gray-800 dark:text-gray-200">Welcome to Ryde</p>
+    <span class="text-sm text-gray-500 dark:text-gray-400">Your Journey Starts here</span>
 </div>
 
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg rounded-[2.5rem]">
             {{ $slot }}
         </div>
     </div>
 </body>
+
+<script>
+    lucide.createIcons();
+</script>
+
 </html>
